c172d6141d335b4df6fbc11a92d6947dd0e1d10b
Alikhan Batchaev <loko-han@mail.ru>
2020-01-28 10:39:52 +0300

    новый файл - SeconViewController

---
+++ BMI Calculator/Controller/SeconViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SeconViewController.swift
+//  BMI Calculator
+//
+//  Created by 🧔🏻Alikhan Batchaev on 28.01.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import Foundation
